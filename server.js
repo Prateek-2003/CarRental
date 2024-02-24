01b06164e146e772f92eb8fc2cc3c7355db9e2dd
Prateek Singh <104994924+Prateek-2003@users.noreply.github.com>
2024-02-24 18:07:51 +0530

    commit

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const port = process.env.PORT || 5000
 const dbConnection = require('./db')
 app.use(express.json())
 
-const path = require('path');
+//const path = require('path');
 
-if(process.env.NODE_ENV==='production')
-{
+// if(process.env.NODE_ENV==='production')
+// {
 
-    app.use('/' , express.static('client/build'))
+//     app.use('/' , express.static('client/build'))
 
-    app.get('*' , (req , res)=>{
+//     app.get('*' , (req , res)=>{
 
-          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+//           res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 
-    })
+//     })
 
-}
+// }
 
 app.get("/", (req, res) => { 
   res.send('Hello World!');
