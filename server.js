3cb0e310c4a14ac658886ed765c370ca623d0fd9
Prateek Singh <104994924+Prateek-2003@users.noreply.github.com>
2024-02-24 18:14:02 +0530

    commit

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 5000
 const dbConnection = require('./db')
 app.use(express.json())
 
+app.use("/api/cars/" , require("./routes/carsRoute"))
+app.use("/api/users/" , require("./routes/usersRoute"))
+app.use("/api/bookings/" , require("./routes/bookingsRoute"))
+
+
 //const path = require('path');
 
 // if(process.env.NODE_ENV==='production')
@@ -23,10 +28,6 @@ app.get("/", (req, res) => {
   res.send('Hello World!');
 });
 
-
-app.use("/api/cars/" , require("./routes/carsRoute"))
-app.use("/api/users/" , require("./routes/usersRoute"))
-app.use("/api/bookings/" , require("./routes/bookingsRoute"))
 
     app.listen(port, () => {
       console.log(`Node JS server started in Port ${port}`);
