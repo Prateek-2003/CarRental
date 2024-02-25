c1808b01f7895f81a89c98a2fdd2c53c48846264
Prateek Singh <104994924+Prateek-2003@users.noreply.github.com>
2024-02-25 20:08:59 +0530

    C

--- client/src/redux/actions/carsAction.js
+++ client/src/redux/actions/carsAction.js
@@ -5,7 +5,7 @@ export const getAllCars = ()=>async dispatch=>{
     dispatch({type: 'LOADING',payload:true})
 
     try{
-        const response = await axios.get("/api/cars/getallcars")
+        const response = await axios.get("https://carrental-2-mk1u.onrender.com/cars/getallcars")
         dispatch({type: 'GET_ALL_CARS',payload:response.data})
         dispatch({type: 'LOADING',payload:false})
     }catch(error){
@@ -19,7 +19,7 @@ export const addCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('/api/cars/addcar' , reqObj)
+         await axios.post('https://carrental-2-mk1u.onrender.com/cars/addcar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('New car added successfully')
@@ -38,7 +38,7 @@ export const editCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('/api/cars/editcar' , reqObj)
+         await axios.post('https://carrental-2-mk1u.onrender.com/cars/editcar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('Car details updated successfully')
@@ -58,7 +58,7 @@ export const deleteCar=(reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING' , payload:true})
 
     try {
-         await axios.post('/api/cars/deletecar' , reqObj)
+         await axios.post('https://carrental-2-mk1u.onrender.com/cars/deletecar' , reqObj)
        
          dispatch({type: 'LOADING' , payload:false})
          message.success('Car deleted successfully')
--- client/src/redux/actions/usersActions.js
+++ client/src/redux/actions/usersActions.js
@@ -6,7 +6,7 @@ export const userLogin = (reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING',payload:true})
 
     try{
-        const response = await axios.post("/api/users/login",reqObj)
+        const response = await axios.post("https://carrental-2-mk1u.onrender.com/users/login",reqObj)
         localStorage.setItem('user',JSON.stringify(response.data))
         message.success('Login Success')
         setTimeout(()=>{
@@ -25,7 +25,7 @@ export const userRegister = (reqObj)=>async dispatch=>{
     dispatch({type: 'LOADING',payload:true})
 
     try{
-        const response = await axios.post("/api/users/register",reqObj)
+        const response = await axios.post("https://carrental-2-mk1u.onrender.com/users/register",reqObj)
         message.success('Registration Successfull')
         setTimeout(()=>{
             window.location.href='/login'   
