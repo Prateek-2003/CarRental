3be3fc2961bb0515bb53e3dd1a4ad992c117c571
Prateek Singh <104994924+Prateek-2003@users.noreply.github.com>
2024-02-25 16:19:06 +0530

    c

--- backened/server.js
+++ backened/server.js
@@ -9,20 +9,20 @@ app.use("/api/users/" , require("./routes/usersRoute"))
 app.use("/api/bookings/" , require("./routes/bookingsRoute"))
 
 
-//const path = require('path');
+const path = require('path');
 
-// if(process.env.NODE_ENV==='production')
-// {
+if(process.env.NODE_ENV==='production')
+{
 
-//     app.use('/' , express.static('client/build'))
+    app.use('/' , express.static('client/build'))
 
-//     app.get('*' , (req , res)=>{
+    app.get('*' , (req , res)=>{
 
-//           res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 
-//     })
+    })
 
-// }
+}
 
 app.get("/", (req, res) => { 
   res.send('Hello World!');
