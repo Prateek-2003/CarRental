bf28d631e2b0f1db2a3d974643c9530c3950bdd1
Prateek Singh <104994924+Prateek-2003@users.noreply.github.com>
2024-02-26 08:15:08 +0530

    c

--- client/src/redux/actions/bookingActions.js
+++ client/src/redux/actions/bookingActions.js
@@ -5,7 +5,7 @@ export const bookCar = (reqObj) =>async dispatch =>{
     dispatch({type: 'LOADING',payload:true})
 
     try{
-        await axios.post("/api/bookings/bookcar", reqObj)
+        await axios.post("https://carrental-2-mk1u.onrender.com/api/bookings/bookcar", reqObj)
         dispatch({type: 'LOADING',payload:false})
         setTimeout(()=>{
             message.success('your car booked successfully')
@@ -22,7 +22,7 @@ export const getAllBookings = ()=>async dispatch=>{
     dispatch({type: 'LOADING',payload:true})
 
     try{
-        const response = await axios.get("/api/bookings/getallbookings")
+        const response = await axios.get("https://carrental-2-mk1u.onrender.com/api/bookings/getallbookings")
         dispatch({type: 'GET_ALL_BOOKINGS',payload:response.data})
         dispatch({type: 'LOADING',payload:false})
     }catch(error){
